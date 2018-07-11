5521bbd3f97931f710736a7a3d62dcf1326080d1
1asdfghjkl7 <jordanwilliams288@gmail.com>
2018-07-11 10:46:04 -0500

    finished kennel

---
+++ src/LocationList.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class LocationList extends Component {
+  render() {
+    return (
+      <div>
+        <h4>Nashville North Location</h4>
+        <h5>500 Puppy Way</h5>
+      </div>
+    );
+  }
+}
