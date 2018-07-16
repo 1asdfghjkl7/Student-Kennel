5e6863d86c8d316974bdb942e15f4ae52d8aef52
1asdfghjkl7 <jordanwilliams288@gmail.com>
2018-07-16 16:36:55 -0500

    started chapter 9

--- src/AnimalList.js
+++ src/AnimalList.js
@@ -6,6 +6,32 @@ export default class AnimalList extends Component {
     animals: []
   };
 
+  addAnimal = () => {
+    const inputName = document.getElementById("inputName").value;
+    const inputBreed = document.getElementById("inputBreed").value;
+
+    const animalObject = {
+      name: inputName,
+      breed: inputBreed
+    };
+    fetch("http://localhost:5002/animals", {
+      method: "POST",
+      headers: { "Content-Type": "application/json; charset=utf-8" },
+      body: JSON.stringify(animalObject)
+    })
+      .then(() => {
+        console.log("haw");
+        return fetch("http://localhost:5002/animals");
+      })
+      .then(a => a.json())
+      .then(animalList => {
+        this.setState({
+          animals: animalList
+        });
+        console.log("boi");
+      });
+  };
+
   checkOutAnimal = animalId => {
     // Delete the specified animal from the API
     fetch(`http://localhost:5002/animals/${animalId}`, {
@@ -34,6 +60,17 @@ export default class AnimalList extends Component {
   render() {
     return (
       <React.Fragment>
+        <form action="">
+          <input id="inputName" type="text" />
+          <input id="inputBreed" type="text" />
+          <button
+            onClick={() => {
+              this.addAnimal();
+            }}
+          >
+            Submit
+          </button>
+        </form>
         <ul>
           {this.state.animals.map(animal => (
             <Animal
--- src/Employee.js
+++ src/Employee.js
@@ -1,27 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types"
 import "bootstrap/dist/css/bootstrap.min.css";
 
-export default props => {
+const Employee = ({ employee, children }) => {
   return (
     <div className="card" style={{ width: `18rem` }}>
       <div className="card-body">
-        <h5 className="card-title">{props.children}</h5>
+        <h5 className="card-title">{children}</h5>
         {
           <Link
             className="card-link"
             to={{
-              pathname: `/employees/${props.employee.id}`,
-              state: { employee: props.employee }
+              pathname: `/employees/${employee.id}`,
+              state: { employee: employee }
             }}
           >
             Details
           </Link>
         }
-        <a href="#" onClick={() => props.checkOutEmployee(props.employee.id)}>
+        <a href="#" onClick={() => children.checkOutEmployee(employee.id)}>
           Delete
         </a>
       </div>
     </div>
   );
 };
+
+Employee.propTypes = {
+  employee: PropTypes.object
+}
+
+export default Employee
